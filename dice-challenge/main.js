55c2dcedfb389871afc1067a124fcb4c5fe3d8ca
marcandy <marcandy.nj@gmail.com>
2017-04-26 02:58:56 -0500

    oop

--- dice-challenge/main.js
+++ dice-challenge/main.js
@@ -1,58 +1,67 @@
+class Die {
+  constructor(sides) {
+    this.sides = sides || 6; //default 6
+    this.rand = 0;
+    // this.roll = this.roll.bind(this);
+  }
 
-var die = {
-
-  rollOne: function() {
-    this.output = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-    return this.output
-  },
-
-  rollMany: function(num) {
-    this.num = num;
-    this.output = 0
-    for(var i = 1; i <= num; i++) {
-     var random = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-      console.log(random);
-      this.output += random;
-    }
-    return this.output;
-  },
-
-  chance : 1,
-
-  loaded: function(pre) {
-    this.pre = pre;
-
-    this.rand = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-
-
-
-      if(die.chance === 1) {
-      die.chance = 0;
-      console.log(die.chance)
-      return this.pre;
-      }
-
-      else {
-      if(this.rand === this.pre) {
-        console.log('hey')
-        return die.loaded(pre)
-      }
-        else {
-        die.chance = 1;
-        return this.rand;
-      }
-      }
-
-
-
-
-
+  roll() {
+    this.rand = Math.floor(Math.random() * this.sides) + 1;
+    return this.rand
   }
 }
 
-var dice1 = Object.create(die);
+let sixDie = new Die(6);
 
-console.log(dice1.loaded(2));
-console.log(dice1.loaded(2));
-console.log(dice1.loaded(2));
-console.log(dice1.loaded(2));
+
+
+class Cup extends Die {
+  constructor(dice, sides, rand) {
+    super(sides, rand);
+  this.numDice = dice;
+  this.sum = 0;
+
+  }
+
+  rollDice(){
+    for (let i = 1; i <= this.numDice; i++) {
+      console.log(this.rand) //it access rand from parent class
+      this.sum += super.roll();
+
+    }
+    return this.sum;
+  }
+}
+
+let cupTen = new Cup(10);
+cupTen.rollDice();
+
+let loadeDie = new Die();
+
+loadeDie.load = function(pre){
+    this.pre = pre;
+
+    loadeDie.roll();
+
+      if(!loadeDie.chance) {
+
+      loadeDie.chance = true;
+      return this.pre;
+      }
+      else {
+      if(this.rand === this.pre) {
+        console.log('hey')
+        return loadeDie.load(pre)
+      }
+        else {
+        loadeDie.chance = false;
+        return loadeDie.rand;
+      }
+      }
+};
+
+console.log(loadeDie.load(5));
+console.log(loadeDie.load(5));
+console.log(loadeDie.load(5));
+console.log(loadeDie.load(5));
+console.log(loadeDie);
